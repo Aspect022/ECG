3313be9499754160cea82278d1b77f159eb7f85c
Rajathshivraj <rajathdsu@gmail.com>
2026-02-09 22:49:28 +0530

    Initial commit

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,5 @@
 """
-ECG Classification Package
-
-A journal-level ECG classification system using:
-- Convolutional Neural Networks (CNN)
-- Spiking Neural Networks (SNN) 
-- Vision Transformers (ViT)
-- Multiple attention mechanisms
-- Quantization strategies (QAT, PTQ, SNN quantization)
-- Hybrid quantum-classical models (PennyLane)
-
-Optimized for CPU training on AMD Ryzen 5000 series.
+ECG Research Framework
 """
-
 __version__ = "0.1.0"
 __author__ = "ECG Research Team"
-
-from . import data
-from . import models
-from . import quantization
-from . import training
-from . import evaluation
-from . import utils
